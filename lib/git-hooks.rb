9b4ff8bd4758e9e1ee8c375f224992ac7b41de9f
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2014-06-26 19:14:32 -0300

    Add GitHooks::RspecExecutor

--- lib/git-hooks.rb
+++ lib/git-hooks.rb
@@ -2,6 +2,7 @@ require 'git'
 
 require_relative 'git_hooks/pre_commit'
 require_relative 'git_hooks/git'
+require_relative 'git_hooks/rspec_executor'
 require_relative 'git_hooks/rubocop_validator'
 
 module GitHooks
---
+++ spec/git_hooks/rspec_executor_spec.rb
@@ -0,0 +1,36 @@
+module GitHooks
+  describe RspecExecutor do
+    subject(:rspec_executor) { described_class.new }
+
+    describe '#errors?' do
+      subject(:errors?) { rspec_executor.errors? }
+
+      let(:rspec_command) { 'bundle exec rspec --format=progress' }
+
+      before do
+        allow(rspec_executor).to receive(:system).and_return(true)
+      end
+
+      it 'call rubocop on system' do
+        expect(rspec_executor).to receive(:system).with(rspec_command)
+        errors?
+      end
+
+      context 'with broken test' do
+        before do
+          allow(rspec_executor).to receive(:system).and_return(false)
+        end
+
+        it { is_expected.to be_truthy }
+      end
+
+      context 'when files has no offences' do
+        before do
+          allow(rspec_executor).to receive(:system).and_return(true)
+        end
+
+        it { is_expected.to be_falsy }
+      end
+    end
+  end
+end
