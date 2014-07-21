336cd8764aea18c6cc399aa1510bbfbaeef3a9f7
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2014-07-21 14:03:09 -0300

    Fix error on .git_hooks.yml not present

--- lib/git_hooks/config_file.rb
+++ lib/git_hooks/config_file.rb
@@ -2,6 +2,8 @@ module GitHooks
   class ConfigFile
     def initialize(path)
       @content = YAML.load_file(path)
+    rescue Errno::ENOENT
+      @content = {}
     end
 
     def pre_commits
--- spec/git_hooks/config_file_spec.rb
+++ spec/git_hooks/config_file_spec.rb
@@ -6,12 +6,20 @@ module GitHooks
     let(:content) { { 'pre_commits' => pre_commits } }
     let(:pre_commits) { %w(foo bar) }
 
-    before do
-      allow(YAML).to receive(:load_file).and_return(content)
+    context 'when the given file does not exists' do
+      subject(:config) { -> { described_class.new(path) } }
+
+      let(:path) { 'some-not-existent-file' }
+
+      it { is_expected.to_not raise_error }
     end
 
     describe '#pre_commits' do
       subject { config.pre_commits }
+
+      before do
+        allow(YAML).to receive(:load_file).and_return(content)
+      end
 
       it 'has the pre commits specified on hook file' do
         is_expected.to eq(%w(foo bar))
